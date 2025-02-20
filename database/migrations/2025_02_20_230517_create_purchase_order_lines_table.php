724c4366c88737c073e796eede37c31dac8e870a
ZanQuen <andereyan@gmail.com>
2025-02-20 23:16:21 +0800

    Initialize: Purchase Line Table

---
+++ database/migrations/2025_02_20_230517_create_purchase_order_lines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_order_id')->constrained('purchase_orders')->cascadeOnDelete();
+            $table->foreignId('purchase_requisition_item_id')->constrained('purchase_requisition_items')->cascadeOnDelete();
+            $table->foreignId('item_id')->constrained('items')->cascadeOnDelete();
+            $table->unsignedSmallInteger('qty')->length(5);
+            $table->unsignedBigInteger('unit_price');
+            $table->unsignedBigInteger('tax')->nullable()->default(0);
+            $table->unsignedBigInteger('total_price')->nullable();
+            $table->unsignedSmallInteger('received_qty')->length(5)->default(0);
+            $table->unsignedTinyInteger('status')->default(0)->length(1);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_order_lines');
+    }
+};
